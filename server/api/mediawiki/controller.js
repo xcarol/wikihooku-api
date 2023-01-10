6bf55c038ff87e56330ccd76476d3d91ab1b461d
Xavier Carol <xcarolr@gmail.com>
2023-01-10 18:10:04 +0100

    fix: remove unnecessary third parameter 'next'

--- server/api/mediawiki/controller.js
+++ server/api/mediawiki/controller.js
@@ -1,16 +1,16 @@
 const wiki = require('../../services/wiki');
 
 const controller = {
-  async search(req, res, next) {
+  async search(req, res) {
     wiki.request('https://www.wikipedia.org/w/api.php'
       + '?action=opensearch'
       + '&formatversion=2'
       + '&format=json'
       + `&limit=${req.params.limit}`
-      + `&search=${req.params.text}`, res, next);
+      + `&search=${req.params.text}`, res);
   },
-  async page(req, res, next) {
-    wiki.request(`https://www.wikipedia.org/w/api.php?action=parse&prop=wikitext&format=json&page=${req.params.text}`, res, next);
+  async page(req, res) {
+    wiki.request(`https://www.wikipedia.org/w/api.php?action=parse&prop=wikitext&format=json&page=${req.params.text}`, res);
   },
 };
 
