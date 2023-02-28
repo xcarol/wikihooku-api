816def3cb77bfd7bf3977c987de4ce9ec473ca1b
Xavier Carol <xcarolr@gmail.com>
2023-02-28 18:23:09 +0100

    feat: improvement on api calls

--- server/api/mediawiki/controller.js
+++ server/api/mediawiki/controller.js
@@ -5,14 +5,20 @@ const controller = {
     wiki.request('https://www.wikipedia.org/w/api.php'
       + '?action=query'
       + '&format=json'
+      + '&formatversion=2'
       + '&list=search'
-      + '&utf8=1'
-      + '&srnamespace=0'
-      + '&srprop='
-      + `&srsearch=${req.params.text}`, res, next);
+      + '&prop=info'
+      + '&inprop=displaytitle'
+      + `&srsearch=${req.params.text}`
+      + `&sroffset=${req.params.offset}`
+      + '&srinfo=&srprop=', res, next);
   },
   page(req, res, next) {
-    wiki.request(`https://www.wikipedia.org/w/api.php?action=parse&prop=wikitext&format=json&pageid=${req.params.pageid}`, res, next);
+    wiki.request('https://www.wikipedia.org/w/api.php'
+      + '?action=parse'
+      + '&format=json'
+      + '&prop=wikitext'
+      + `&pageid=${req.params.pageid}`, res, next);
   },
 };
 
