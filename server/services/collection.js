9847dd22cb21967ae85a705d89a61eda3561ba6b
Xavier Carol <xcarolr@gmail.com>
2023-05-25 17:27:03 +0200

    feat: add the service to save a collection

--- server/services/collection.js
+++ server/services/collection.js
@@ -6,7 +6,10 @@ const Collection = mongoose.model('Collection', CollectionSchema);
 
 const service = {
   async findCollectionById(collectionId) {
-    return Collection.findOne({ type: collectionId });
+    return Collection.findOne({ collectionId });
+  },
+  async addCollection(name, items) {
+    return Collection.create({ name, items });
   },
 };
 
